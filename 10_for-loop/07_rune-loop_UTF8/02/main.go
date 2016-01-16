0f04e23606707436d7c8b8bf005004b6139c2d6f
Roger Sherman <rsherman@viddler.com>
2016-01-15 23:07:18 -0500

    Runes!

--- 10_for-loop/07_rune-loop_UTF8/02/main.go
+++ 10_for-loop/07_rune-loop_UTF8/02/main.go
@@ -4,6 +4,6 @@ import "fmt"
 
 func main() {
 	for i := 50; i <= 140; i++ {
-		fmt.Println(i, " - ", string(i), " - ", []byte(string(i)))
+		fmt.Printf("%v - %v - %v \n", i, string(i), []byte(string(i)))
 	}
 }
