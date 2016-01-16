b932f0677fee2a28638327b69685036d0483d1bf
Roger Sherman <rsherman@viddler.com>
2016-01-15 23:40:08 -0500

    Still more runes.

--- 10_for-loop/07_rune-loop_UTF8/02/main.go
+++ 10_for-loop/07_rune-loop_UTF8/02/main.go
@@ -6,4 +6,11 @@ func main() {
 	for i := 50; i <= 140; i++ {
 		fmt.Printf("%v - %v - %v \n", i, string(i), []byte(string(i)))
 	}
+	/* This following block shows what happens when you use single quotes - the
+	string gets interpreted as a rune, and its decimal code is outputted.
+	 */
+	foo := 'a'
+	fmt.Println(foo)
+	fmt.Printf("%T \n", foo)
+	fmt.Printf("%T \n", rune(foo))
 }
