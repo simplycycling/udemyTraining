552f4658b3170795270a204dccb99bb3e9e40c57
Roger Sherman <rsherman@viddler.com>
2016-01-23 14:31:36 -0500

    recursion and defer

--- 14_functions/14_defer/01_no-defer/main.go
+++ 14_functions/14_defer/01_no-defer/main.go
@@ -1,17 +1,18 @@
 package main
+
 import "fmt"
 
-func hello()  {
+func hello() {
 	fmt.Print("Hello ")
 
 }
 
-func world()  {
+func world() {
 	fmt.Println("world")
 
 }
 
-func main()  {
+func main() {
 	world()
 	hello()
 
--- 14_functions/14_defer/02_with-defer/main.go
+++ 14_functions/14_defer/02_with-defer/main.go
@@ -2,19 +2,18 @@ package main
 
 import "fmt"
 
-func hello()  {
+func hello() {
 	fmt.Print("Hello ")
 
 }
 
-func world()  {
+func world() {
 	fmt.Println("world")
 
 }
 
-func main()  {
+func main() {
 	defer world()
 	hello()
 
 }
-
